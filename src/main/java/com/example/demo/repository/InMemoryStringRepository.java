c150d148ba9f2dc6a0939a27da9d197d5d143790
Grimax <grishanin.maxim.group@gmail.com>
2021-09-29 18:41:37 +0300

    fix interface

--- src/main/java/com/example/demo/repository/InMemoryStringRepository.java
+++ src/main/java/com/example/demo/repository/InMemoryStringRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.repository;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface InMemoryStringRepository {
 
     List<String> save(List<String> newStrings);
